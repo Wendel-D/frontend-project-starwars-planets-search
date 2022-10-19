1570f5c877e6cfc12a792297b73fe132d1d4a497
wendel-D <wendeldaniel263@gmail.com>
2022-10-19 20:59:20 -0300

    requisito 1 ok

---
+++ src/context/myProvider.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import propTypes from 'prop-types';
+import MyContext from './myContext';
+
+function Provider({ children }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      try {
+        const url = 'https://swapi.dev/api/planets';
+        const request = await fetch(url);
+        const { results } = await request.json();
+        const filterResults = results.filter((e) => delete e.residents);
+        setData(filterResults);
+      } catch (e) {
+        throw new Error(e);
+      }
+    };
+    fetchAPI();
+  }, []);
+
+  const contexto = useMemo(
+    () => ({
+      data,
+    }),
+  );
+
+  return (
+    <MyContext.Provider value={ contexto }>
+      {children}
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: propTypes.shape(),
+}.isRequired;
+
+export default Provider;
