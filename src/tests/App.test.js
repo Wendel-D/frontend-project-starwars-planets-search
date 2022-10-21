b04a1a0a79fbd80d3bd7b435a0384176da7d90ea
wendel-D <wendeldaniel263@gmail.com>
2022-10-21 11:48:13 -0300

    correções

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { findByText, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../App';
 import userEvent from '@testing-library/user-event';
 
